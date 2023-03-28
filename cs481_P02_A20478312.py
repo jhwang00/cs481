68a6ef89661624e6e3ab8937c3a51e34e46b531a
jhwang00 <97809956+jhwang00@users.noreply.github.com>
2023-03-28 17:12:53 -0500

    Update cs481_P02_A20478312.py

--- cs481_P02_A20478312.py
+++ cs481_P02_A20478312.py
@@ -2,10 +2,10 @@ import csv
 import sys
 import nltk
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
 #import string
 
 t = []
-vocab = []
 pos = []
 neut = []
 neg = []
@@ -46,6 +46,7 @@ def preprocessStop(text): #stopwords
     return filtered_text
 
 def vocabb(text): #extracting vocab list from training set(text)
+    vocab = []
     for i in range(len(text)):
         for j in range(len(text[i])):
             if not vocab:
@@ -57,23 +58,45 @@ def vocabb(text): #extracting vocab list from training set(text)
                     vocab.append(text[i][j])
             else:
                 vocab.append(text[i][j])
+    return vocab
 
 #need number of pos/neut/neg
 #need to count of occurence of each words in pos/neut/neg
 #count total words (1s in bag of words)
 
-def bow():
-    pass
+def bag_of_words(data, vocab):
+    # Takes vocabulary list as additional input
+    # Extract the text and labels from dataframe
+    X_text = data['text']
+    y = data['airline_sentiment']
+    
+    # Create a CountVectorizer object and fit it to the training data
+    if vocab is None:
+        vectorizer = CountVectorizer()
+    else:
+        vectorizer = CountVectorizer(vocab = vocab)
+    vectorizer.fit(X_text)
+    
+    # Convert text data to a Bag of Words representation
+    X = vectorizer.transform(X_text)
+    
+    return X, y, vectorizer.get_feature_names()
+
+def bow(voca, sentence):
+    dict = {}
+    for i in range(len(sentence)):
+        exists = 0
+        if sentence[i] in voca and sentence[i] not in dict:
+            exists = 1
+        if exists == 0:
+            dict[sentence[i]] = 0
+        else:
+            dict[sentence[i]] = 1 
+    return dict
+    # tot num etc as list
 
 def positive(text): #input every positive
-    for i in range(len(text)):
-        for j in range(len(text[i])):
-            exists = 0
-            if text[i][j] in vocab:
-                exists = 1
-            if exists == 0:
-                vocab[text[i][j]] = 1
-    return # tot num etc as list
+    pass
 
 def neutral():
     pass
@@ -82,12 +105,17 @@ def negative():
     pass
 
 def main():
+    voca = []
     l = []
-    for i in range(1, int((len(t)-1)*80/100)):
+    for i in range(1, 11):
         l.append(preprocessStop(preprocessStem(preprocessLC(t[i][10]))))
-    vocabb(l)
-    print(vocab)
-    print(int((len(t)-1)*80/100))
+        voca.append(preprocessStop(preprocessStem(preprocessLC(t[i][10]))))
+    #bag_of_words(t[i][10])
+    #print(vocabb(voca))
+    for i in range(len(l)):
+        #print(bag_of_words(l[i], voca))
+        print(bow(voca, l[i]))
+    #print(int((len(t)-1)*80/100))
 
     #preprocess
     #train based on t[i][1]::sentiment
