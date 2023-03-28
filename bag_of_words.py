946a8f8d3f69dec49238d6d29ca28d1066a0ef47
Nick C <90653583+Nyx-AC3@users.noreply.github.com>
2023-03-28 17:01:40 -0500

    Add files via upload

--- bag_of_words.py
+++ bag_of_words.py
@@ -1,12 +1,16 @@
 from sklearn.feature_extraction.text import CountVectorizer
 
-def bag_of_words(data):
+def bag_of_words(data, vocab):
+    # Takes vocabulary list as additional input
     # Extract the text and labels from dataframe
     X_text = data['text']
     y = data['airline_sentiment']
     
     # Create a CountVectorizer object and fit it to the training data
-    vectorizer = CountVectorizer()
+    if vocab is None:
+        vectorizer = CountVectorizer()
+    else:
+        vectorizer = CountVectorizer(vocab = vocab)
     vectorizer.fit(X_text)
     
     # Convert text data to a Bag of Words representation
